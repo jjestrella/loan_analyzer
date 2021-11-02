13548ef2340ab3d1d34d10273f85b4072fa20ca7
jjestrella <estrella.jjuan@gmail.com>
2021-11-01 21:26:34 -0700

    finished csv write code

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -211,3 +211,12 @@ output_path = Path("inexpensive_loans.csv")
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
 # YOUR CODE HERE!
+csvpath = output_path
+with open(csvpath, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    # Write our header row first!
+    csvwriter.writerow(header)
+    for row in inexpensive_loans:
+        csvwriter.writerow(row.values())
+        print(row)
